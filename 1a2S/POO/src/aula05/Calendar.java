c0dbb0089d0d0ecadc98ee8d89aef6cd15ddc1eb
Gonçalo Martins <goncalobmartins@ua.pt>
2023-03-16 17:57:06 +0000

    Calendar

--- 1a2S/POO/src/aula05/Calendar.java
+++ 1a2S/POO/src/aula05/Calendar.java
@@ -1,4 +1,27 @@
 package aula05;
 
 public class Calendar {
+    private int year;
+    private int firstOfYear;
+    private int[][] events;
+
+    public Calendar( int year , int firstOfYear){
+        this.year = year;
+        this.firstOfYear = firstOfYear;
+        this.events = new int[366][3];
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public int getFirstOfYear(){
+        return this.firstOfYear;
+    }
+
+    public void printMonth(int month){
+        String[] monthNames = {"Janeiro" , "Fevereiro" , "Março" , "Abril" , "Maio" , "Junho" , "Julho" , "Agosto" , "Setembro" , "Outubro" , "Novembro" , "Dezembro"};
+        System.out.printf("\n%s %d" , monthNames[month-1] , year);
+        System.out.println("Dom Seg Ter Qua Qui Sex Sab");
+    }
 }
