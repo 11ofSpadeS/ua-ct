75fd8c1f6ed277f23f9b93910d365323e5b7882b
Gonçalo Martins <goncalobmartins@ua.pt>
2023-03-31 14:26:07 +0100

    POO aula06 ?

--- 1a2S/POO/src/aula06/ContactMenu.java
+++ 1a2S/POO/src/aula06/ContactMenu.java
@@ -5,8 +5,9 @@ import aula05.DateYMD;
 import java.util.Scanner;
 
 public class ContactMenu {
+    private static Contact[] contacts;
+    private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         while (true) {
             System.out.println("MENU:");
             System.out.println("1 - Add New Contact");
@@ -20,29 +21,153 @@ public class ContactMenu {
             if (option == 0)
                 break;
             switch (option) {
-                case 1:
-                    System.out.println("Insira o numero de telemovel:");
+                case 1 -> {
+                    System.out.print("Insira o nome: ");
+                    String name = input.nextLine();
+                    System.out.print("Insira o cc: ");
+                    String ccStr = input.nextLine();
+                    int cc = Integer.parseInt(ccStr);
+                    System.out.print("Insira a data de nascimento (formado: dd-mm-yyyy): ");
+                    String date = input.nextLine();
+                    String[] dateParts = date.split("-");
+                    DateYMD birthDate = new DateYMD(Integer.parseInt(dateParts[0]), Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[2]));
+                    System.out.print("Insira o email: ");
+                    String mail = input.nextLine();
+                    System.out.print("Insira o telefone: ");
                     int cellnumber = input.nextInt();
-                    System.out.println("Insira o email: ");
-                    String email = input.next();
-                    System.out.println("Insira o nome:");
-                    String name = input.next();
-                    break;
-                case 2:
+                    Pessoa person = new Pessoa(name, cc, birthDate);
+                    Contact contact = new Contact(person, cellnumber, mail);
 
-                    break;
-                case 3:
+                    if (contacts == null) {
+                        contacts = new Contact[1];
+                        contacts[0] = contact;
+                    }
+                }
+                case 2 -> {
+                    System.out.print("Insira o nome, email ou telefone do contacto que pretende alterar: ");
+                    String query = input.nextLine();
+                    int[] indexes = searchContactsIndex(query);
+                    System.out.printf("Contactos encontrados: %d%n", indexes.length);
 
-                    break;
-                case 4:
+                    if (indexes.length == 0) {
+                        System.out.println("Não foram encontrados contactos com esse nome, email ou telefone");
+                    } else if (indexes.length == 1) {
+                        System.out.printf("Alterando contacto: %s%n", contacts[indexes[0]]);
+                        System.out.print("Insira o novo telefone: ");
+                        int phone = input.nextInt();
+                        System.out.print("Insira o novo email: ");
+                        String email = input.nextLine();
+                        contacts[indexes[0]].setCellnumber(phone);
+                        contacts[indexes[0]].setEmail(email);
+                    } else {
+                        Contact contact = selectContact(indexes);
+                        System.out.printf("Alterando contacto: %s%n", contact);
+                        System.out.print("Insira o novo telefone: ");
+                        int phone = input.nextInt();
+                        System.out.print("Insira o novo email: ");
+                        String email = input.nextLine();
+                        contact.setCellnumber(phone);
+                        contact.setEmail(email);
+                    }
+                }
 
-                    break;
-                case 5:
 
-                    break;
-                default:
-                    System.out.println("Invalid option");
+                case 3 -> {
+                    System.out.print("Insira o nome, email ou telefone do contacto que pretende alterar: ");
+                    String query = input.nextLine();
+                    int[] indexes = searchContactsIndex(query);
+                    System.out.printf("Contactos encontrados: %d%n", indexes.length);
+
+                    if (indexes.length == 0) {
+                        System.out.println("Não foram encontrados contactos com esse nome, email ou telefone");
+                    } else if (indexes.length == 1) {
+                        System.out.printf("Apagando contacto: %s%n", contacts[indexes[0]]);
+                        Contact[] newContacts = new Contact[contacts.length - 1];
+                        System.arraycopy(contacts, 0, newContacts, 0, indexes[0]);
+                        System.arraycopy(contacts, indexes[0] + 1, newContacts, indexes[0], contacts.length - indexes[0] - 1);
+                        contacts = newContacts;
+                    } else {
+                        Contact contact = selectContact(indexes);
+                        System.out.printf("Apagando contacto: %s%n", contact);
+                        Contact[] newContacts = new Contact[contacts.length - 1];
+                        System.arraycopy(contacts, 0, newContacts, 0, contact.getId());
+                        System.arraycopy(contacts, contact.getId() + 1, newContacts, contact.getId(), contacts.length - contact.getId() - 1);
+                        contacts = newContacts;
+                    }
+                }
+                case 4 -> {
+                    System.out.print("Insira o nome, email ou telefone do contacto que pretende alterar: ");
+                    String query = input.nextLine();
+                    int[] indexes = searchContactsIndex(query);
+                    System.out.printf("Contactos encontrados: %d%n", indexes.length);
+
+                    if (indexes.length == 0) {
+                        System.out.println("Não foram encontrados contactos com esse nome, email ou telefone");
+                    } else if (indexes.length == 1) {
+                        System.out.println(contacts[indexes[0]]);
+                    } else {
+                        for (int index : indexes)
+                            System.out.printf("%s%n", contacts[index]);
+                    }
+                    System.out.println();
+                }
+                case 5 -> {
+                    for (Contact contact : contacts)
+                        System.out.printf("%s%n", contact);
+                    System.out.println();
+                }
+
+
+                default -> {
+                    System.out.println("Opção inválida!");
+                }
             }
         }
     }
-}
+                    private static boolean checkIfContactExists(int cc) {
+                    for (Contact c : contacts) {
+                        if (c.getPerson().getCc() == cc) {
+                            System.out.print("Já existe um contacto para essa pessoa, pretende criar um novo? (s/n): ");
+                            String answer = input.nextLine();
+                            return answer.equals("s");
+                        }
+                    }
+                    return true;
+                }
+
+                private static int[] searchContactsIndex(String query) {
+                    int[] indexes = new int[contacts.length];
+                    int index = 0;
+                    for (int i = 0; i < contacts.length; i++) {
+                        if (contacts[i].getPerson().getName().contains(query) || contacts[i].getMail().contains(query) || contacts[i].getCellNumber().contains(query)) {
+                            indexes[index] = i;
+                            index++;
+                        }
+                    }
+                    return indexes;
+                }
+
+                private static Contact selectContact(int[] indexes) {
+                    System.out.println("Foram encontrados vários contactos com esse nome, email ou telefone, selecione um:");
+                    for (int index : indexes)
+                        System.out.printf("%s%n", contacts[index]);
+
+                    Contact contact;
+                    do {
+                        System.out.print("Insira o ID do contacto que pretende alterar: ");
+                        int index = input.nextInt();
+                        contact = searchContactById(index);
+                    } while (contact == null);
+
+                    return contact;
+                }
+
+                private static Contact searchContactById(int id) {
+                    for (Contact contact : contacts) {
+                        if (contact.getId() == id) {
+                            return contact;
+                        }
+                    }
+                    return null;
+                }
+            }
---
+++ 1a2S/POO/src/aula06/Vector.java
@@ -0,0 +1,91 @@
+package aula06;
+
+public class Vector {
+    private int[] vector;
+
+    public Vector() {
+        this.vector = new int[0];
+    }
+    public Vector(int size) {
+        this.vector = new int[size];
+    }
+    public Vector(int[] vector) {
+        this.vector = vector;
+    }
+    public int[] getVector() {
+        return vector;
+    }
+
+    public int size() {
+        return this.vector.length;
+    }
+    public boolean contains(int value) {
+        for (int n : this.vector)
+            if (n == value)
+                return true;
+        return false;
+    }
+    public int count(int value) {
+        int count = 0;
+        for (int n : this.vector)
+            if (n == value)
+                count++;
+        return count;
+    }
+
+    public void insert(int value) {
+        if (this.contains(value)) return;
+        int[] aux = new int[this.size()+1];
+        System.arraycopy(this.vector, 0, aux, 0, this.size());
+        aux[this.size()] = value;
+        this.vector = aux;
+    }
+    public void remove(int value) {
+        int[] aux = new int[this.size()-this.count(value)];
+        int i = 0;
+        for (int n : this.vector) {
+            if (n == value)
+                continue;
+            aux[i] = n;
+            i++;
+        }
+        this.vector = aux;
+    }
+    public void empty() {
+        this.vector = new int[0];
+    }
+
+    public Vector add(Vector secondVector) {
+        Vector result = new Vector();
+        for (int n : this.vector)
+            if (!result.contains(n))
+                result.insert(n);
+        for (int n : secondVector.vector)
+            if (!result.contains(n))
+                result.insert(n);
+        return result;
+    }
+    public Vector subtract(Vector secondVector) {
+        Vector result = new Vector();
+        for (int n : this.vector)
+            if (!result.contains(n) && !secondVector.contains(n))
+                result.insert(n);
+        return result;
+    }
+    public Vector intersect(Vector secondVector) {
+        Vector result = new Vector();
+        for (int n : this.vector)
+            if (!result.contains(n) && secondVector.contains(n))
+                result.insert(n);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (int n : this.vector)
+            result.append(String.format("%d ", n));
+        return this.size() > 0 ? result.substring(0, result.length()-1) : result.toString();
+    }
+}
+
